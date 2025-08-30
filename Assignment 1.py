8b4bdcead5173c1871a002267f319bf820973210
Prakriti Kesharwani <141558090+pra-kritii@users.noreply.github.com>
2025-08-30 17:46:30 +0530

    Add files via upload

---
+++ Assignment 1.py
@@ -0,0 +1,14 @@
+#Task 1
+a=int(input("Enter the first numbers: "))
+b= int(input("Enter the second number: "))
+print("Addition: ",a+b)
+print("Subtraction: ",a-b)
+print("Multiplication: ",a*b)
+print("Division: ",a/b)
+
+
+#Task 2
+x=input("Enter you first name: ")
+y=input("Enter your last name: ")
+
+print("Hello,",x , y,"! Welcome to the Python program")
